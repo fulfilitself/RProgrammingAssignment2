23c0256f891cc580044fae1b3a7e5a691268af87
fulfilitself <fulfilitself@yahoo.com>
2015-03-22 18:53:28 -0400

    add cachematrix.R file to remote repo

---
+++ cachematrix.R
@@ -0,0 +1,30 @@
+## This function create a matrix and some associated sub-functions/methods
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y 
+                m <<- NULL 
+        }
+        get <- function() x 
+        setinverse <- function(inverse) m <<- inverse 
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
+
+## This function calculates the inverse of the special "matrix" created with makeCacheMatrix.
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}
+
